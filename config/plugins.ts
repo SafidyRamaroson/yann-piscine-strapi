66dffa9c75eb21a96bba157229a9349614852f50
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-06-16 13:07:01 +0200

    Update email settings in plugins configuration; change default sender email and reply-to address

--- config/plugins.ts
+++ config/plugins.ts
@@ -21,9 +21,9 @@ module.exports = ({ env }) => ({
 				apiKey: env('API_KEY'),
 			  },
 			settings: {
-				defaultSenderEmail: 'safidyrm.pro@gmail.com',
+				defaultSenderEmail: 'lespiscinesdeyann@gmail.com',
         		defaultSenderName: 'yannpiscines.com',
-        		defaultReplyTo: 'safidyrm.pro@gmail.com',
+        		defaultReplyTo: 'noreply@yannpiscines.com',
 			},
 		},
 	},
